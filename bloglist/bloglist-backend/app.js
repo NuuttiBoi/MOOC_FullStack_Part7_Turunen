150743277f903584991821f5331319a4283a16f5
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2026-04-21 16:28:24 +0300

    7.7.

--- bloglist/bloglist-backend/app.js
+++ bloglist/bloglist-backend/app.js
@@ -1,23 +1,29 @@
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
 const app = express()
-const Blog = require("./models/blog")
-const {totalLikes} = require("./utils/list_helper");
-const logger = require("./utils/logger")
 
 logger.info('Connecting to', config.MONGODB_URI)
 mongoose
-    .connect(config.MONGODB_URI, {family: 4})
+    .connect(config.MONGODB_URI, { family: 4 })
     .then(() => {
         logger.info('connected to MongoDB')
     })
     .catch((error) => {
         logger.error('error connecting to MongoDB', error.message)
     })
-app.use(express.static('dist'))
+
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+
+app.use(express.static(path.join(__dirname, '../bloglist-frontend/dist')))
+
+app.get('/{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../bloglist-frontend/dist/index.html'))
+})
 
 module.exports = app
